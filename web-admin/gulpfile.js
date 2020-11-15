125173a47b597b65de786f41bc7f0007f5e7fa26
Anton Kuzmichev <assargin@gmail.com>
2020-11-15 22:09:27 +0300

    deploy: gulp explicit filenames

--- web-admin/gulpfile.js
+++ web-admin/gulpfile.js
@@ -13,7 +13,10 @@ gulp.task('clean', function(){
 });
 
 gulp.task('styles', function(){
-    return gulp.src(['css/*.css'])
+    return gulp.src([
+        'css/main.css',
+        'css/loader.css',
+    ])
         .pipe(concat('app.min.css'))
         .pipe(cleanCSS({
             keepBreaks: true
@@ -25,7 +28,11 @@ gulp.task('styles', function(){
 });
 
 gulp.task('deps-styles', function(){
-    return gulp.src(['css/lib/*.css'])
+    return gulp.src([
+        'css/lib/bootstrap-4.3.1.min.css',
+        'css/lib/font-awesome-4.6.2.min.css',
+        'css/lib/toastr.min.css'
+    ])
         .pipe(concat('deps.min.css'))
         .pipe(cleanCSS({
             keepBreaks: true
@@ -37,7 +44,20 @@ gulp.task('deps-styles', function(){
 });
 
 gulp.task('scripts', function(){
-    return gulp.src(['js/*.js'])
+    return gulp.src([
+        'js/janus.js',
+        'js/utils.js',
+        'js/debug-utils.js',
+        'js/cheat-codes.js',
+        'js/session-monitoring.js',
+        'js/ui.js',
+        'js/remote-chat.js',
+        'js/commands.js',
+        'js/remote-video.js',
+        'js/video-stats.js',
+        'js/gesture-builder.js',
+        'js/remote-admin.js'
+    ])
         .pipe(concat('app.min.js'))
         .pipe(terser())
         .pipe(size({
@@ -47,7 +67,14 @@ gulp.task('scripts', function(){
 });
 
 gulp.task('deps-scripts', function() {
-    return gulp.src(['js/lib/*.js'])
+    return gulp.src([
+        'js/lib/jquery-3.3.1.min.js',
+        'js/lib/bootstrap-4.3.1.min.js',
+        'js/lib/adapter-6.4.0.min.js',
+        'js/lib/popper-2.5.3.min.js',
+        'js/lib/bootbox-5.4.0.min.js',
+        'js/lib/toastr.min.js'
+    ])
         .pipe(concat('deps.min.js'))
         .pipe(size({
             title: 'Size of JS libs'
